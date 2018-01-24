15a4afb43de83d0c975875886667630a8bd9f53c
Nir <nirhalay@gmail.com>
2018-01-24 19:19:54 +0200

    Example: press any key to quit

--- src/com/jdht/example/Main.java
+++ src/com/jdht/example/Main.java
@@ -6,6 +6,7 @@ package com.jdht.example;
 
 import org.jdht.dht.*;
 
+
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.util.LinkedList;
@@ -13,53 +14,43 @@ import java.util.List;
 
 public class Main {
 
-    private static boolean stopped = false;
-
     public static void main(String[] args) {
 
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
+        try {
 
-                    DHTPeersReceiver peersReceiver = new DHTPeersReceiver() {
-                        @Override
-                        public void gotPeersFromDHT(ByteBuffer infoHash, List<PeerInfo> peers) {
-                            for (PeerInfo peer : peers)
-                                Logger.log("GOT PEER " + peer.getIp() + " FOR INFOHASH " + Logger.toHex(infoHash.array()));
-                        }
-                    };
-
-                    //byte[] nodeID = hexStringToByteArray("653DBD6A8632865B26AF8FC9861339C49C03F4F6");
-
-                    //DHT dht = new DHT(new Node(ByteBuffer.wrap(nodeID), 5739), 5643, peersReceiver);
-                    DHT dht = new DHT(5739, 5643, peersReceiver);
-                    dht.start();
-
-                    List<Node> list = new LinkedList<>();
-                    list.add(new Node( InetAddress.getByName("router.bittorrent.com"), 6881, true));
-                    list.add(new Node( InetAddress.getByName("dht.transmissionbt.com"), 6881, true));
-                    list.add(new Node( InetAddress.getByName("router.utorrent.com"), 6881, true));
-                    dht.addNodes(list);
-
-
-                    byte[] infohash = hexStringToByteArray("653DBD6A8632865B26AF8FC9861339C49C034775");
-
-                    dht.getPeers(ByteBuffer.wrap(infohash), false);
-
-                    while(!stopped){
-                        Thread.sleep(3000);
-                    }
-
-                    dht.stop();
-
-                } catch (Exception e) {
-                    e.printStackTrace();
+            DHTPeersReceiver peersReceiver = new DHTPeersReceiver() {
+                @Override
+                public void gotPeersFromDHT(ByteBuffer infoHash, List<PeerInfo> peers) {
+                    for (PeerInfo peer : peers)
+                        Logger.log("GOT PEER " + peer.getIp() + " FOR INFOHASH " + Logger.toHex(infoHash.array()));
                 }
-            }
-        });
+            };
 
-        thread.start();
+            //byte[] nodeID = hexStringToByteArray("653DBD6A8632865B26AF8FC9861339C49C03F4F6");
+
+            //DHT dht = new DHT(new Node(ByteBuffer.wrap(nodeID), 5739), 5643, peersReceiver);
+            DHT dht = new DHT(5739, 5643, peersReceiver);
+            dht.start();
+
+            List<Node> list = new LinkedList<>();
+            list.add(new Node( InetAddress.getByName("router.bittorrent.com"), 6881, true));
+            list.add(new Node( InetAddress.getByName("dht.transmissionbt.com"), 6881, true));
+            list.add(new Node( InetAddress.getByName("router.utorrent.com"), 6881, true));
+            dht.addNodes(list);
+
+
+            byte[] infohash = hexStringToByteArray("653DBD6A8632865B26AF8FC9861339C49C034775");
+
+            dht.getPeers(ByteBuffer.wrap(infohash), false);
+
+            // Press any key to quit
+            System.in.read();
+
+            dht.stop();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
